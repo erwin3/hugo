cb636f0fb3bcfe557c9cfcf789bcb84a6bde73a7
hmaelzer <hmaelzer@gmx.de>
2013-05-21 00:23:13 +0200

    refs #4 verfügbare Figuren in einem Querbalken oben zur Verfügung stellen

--- scripts/figure.functionality.js
+++ scripts/figure.functionality.js
@@ -4,28 +4,35 @@
 
 function ActivateFunctionality() 
 {
+	var count = 1 ;
 	$(".figure").click(function () 
  {
    // copy the figure, atribute false deletes the click-event
-   var copy = $(this).clone(false);
+   var copy1 = $(this).clone(false);
+   var copy2 = $(this).clone(false);   
+   var newId = "fig_" + count + "_"+ copy1.find('img').attr("alt") ;
    
    // add a unique ID
-   copy.attr("id", copy.attr("id") + "-1");
+   copy1.attr("id", newId );
+   copy2.attr("id", newId );
+   count = count+1;
    
    // remove class figure as we dont want a duplicate of this copy
-   copy.removeClass("figure");
+   copy1.removeClass("figure");
+   copy2.removeClass("figure");
    
    // add the class figureOnBoard                    
-   copy.addClass("figureOnBoard");
+   copy1.addClass("figureOnBoard");
+   copy2.addClass("figureOnBoard");
    
    //copy.addClass("dead");
    
    // set draggable - so it can be dragged - jquery UI
-   copy.draggable();
-   copy.find('img').css('width' , '50px');
-   copy.find('img').css('height', '50px');
+   copy1.draggable();
+   copy1.find('img').css('width' , '50px');
+   copy1.find('img').css('height', '50px');
    
-   copy.bind('click', function () 
+   copy1.bind('click', function () 
    {
    	  // rotate on click
    	  if ($(this).hasClass('dead'))
@@ -38,8 +45,15 @@ function ActivateFunctionality()
    	  	}
    	});
    	
+   var textArea = document.createElement("textarea");
+   textArea.style.width = "500px" ;
+   textArea.style.maxWidth = "500px" ;
+   textArea.style.height = "50px" ;
+   	copy2.append(textArea);
+   	
    	// add this copy to the container div
-   	$("#figureadder").append(copy);
+   	$("#figureadder").append(copy1);
+   	$("#figurepanel").append(copy2);
  });
 
  $(".figureOnBoard").click(function () 
